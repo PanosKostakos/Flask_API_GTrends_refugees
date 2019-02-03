c0415c8089028836d7fa5ec2238433a3a35e3d8e
Panos <19819500+PanosKostakos@users.noreply.github.com>
2019-02-03 18:38:34 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,16 +6,8 @@ import pickle
 import requests
 import json
 
-# We train a model to predict the levels of refugge arrivals in the Greek islands
-# based on the google searches for Facebook in the Turkish region of Balikesir Province the week before.
-
 
 dataset = pd.read_csv('DF.csv', sep= ';')
-
-
-
-#rescale the vol of arrivales in order to have 
-#  the magnitude of values in x and y 
 
 X = dataset.iloc [:, [96]].values
 
@@ -33,8 +25,6 @@ pickle.dump(regressor, open('model.pkl','wb'))
 
 
 model = pickle.load(open('model.pkl','rb'))
-
-#print(model.predict([[sys.argv[1]]]))
 
 
 
